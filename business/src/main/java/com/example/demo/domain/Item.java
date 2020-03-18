c0f4d23d585b095511e6290931459ec3469f4013
iseyoshitaka <ise0615@gmail.com>
2020-03-19 01:31:54 +0900

    日本語に変更

--- business/src/main/java/com/example/demo/domain/Item.java
+++ business/src/main/java/com/example/demo/domain/Item.java
@@ -7,15 +7,15 @@ import javax.validation.constraints.Size;
 
 public class Item {
 	private Long id;
-	
-	@NotBlank(message="商品名不能为空")
+
+	@NotBlank(message = "商品名は入力必須です")
 	private String name;
-	
-	@Min(value=10, message="不能小于10")
-	@Max(value=10000, message="不能大于10000")
+
+	@Min(value = 10, message = "価格は10より大きい値を入力して下さい")
+	@Max(value = 10000, message = "価格は10000より小さい値を入力して下さい")
 	private float price;
-	
-	@Size(max=50, message="生产商长度不能超过50")
+
+	@Size(max = 50, message = "生産者は50文字以内で入力して下さい")
 	private String vendor;
 
 	public Long getId() {
