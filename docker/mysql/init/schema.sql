4dd1a0497821a237895c76a3a27c1e2579ba1044
iseyoshitaka <iseyoshitaka@team-lab.com>
2020-03-19 13:11:49 +0900

    Mysqlに接続成功

--- docker/mysql/init/schema.sql
+++ docker/mysql/init/schema.sql
@@ -1,7 +1,12 @@
+
+CREATE DATABASE IF NOT EXIST sample DEFAULT CHARACTER SET utf8mb4;
+
+USE sample;
+
 CREATE TABLE IF NOT EXISTS item (
   id bigint(20) NOT NULL AUTO_INCREMENT,
   name varchar(255),
   price real,
   vendor varchar(255),
-  PRIMARY KEY (id),
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
