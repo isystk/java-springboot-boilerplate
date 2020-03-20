d5f23ca89c59379d45433e5c0d89090b5d1a404b
iseyoshitaka <iseyoshitaka@team-lab.com>
2020-03-20 23:07:10 +0900

    APO試行錯誤中

--- web-admin/src/main/java/com/isystk/sample/web/admin/controller/html/home/HomeController.java
+++ web-admin/src/main/java/com/isystk/sample/web/admin/controller/html/home/HomeController.java
@@ -1,5 +1,7 @@
 package com.isystk.sample.web.admin.controller.html.home;
 
+import static com.isystk.sample.web.base.WebConst.*;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.isystk.sample.web.base.controller.html.AbstractHtmlController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
-@RequestMapping("/")
+@Slf4j
+@RequestMapping(ADMIN_HOME_URL)
 public class HomeController extends AbstractHtmlController {
+
+	@Override
+	public String getFunctionName() {
+		return "HOME";
+	}
 
 	@GetMapping
 	public String index(Model model) {
---
+++ web/src/main/java/com/isystk/sample/web/base/security/authorization/Authorizable.java
@@ -0,0 +1,11 @@
+package com.isystk.sample.web.base.security.authorization;
+
+public interface Authorizable {
+
+    /**
+	 * 認可を必要とする機能の場合はtrueを返します。
+	 *
+	 * @return
+	 */
+    boolean authorityRequired();
+}
