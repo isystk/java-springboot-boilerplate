f375d4edcfa9cfc618581bb1b2b3925c63f86894
iseyoshitaka <iseyoshitaka@team-lab.com>
2020-03-20 11:23:33 +0900

    Apache連携＆SSL対応

---
+++ web-admin/src/main/java/com/example/demo/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+
+import org.springframework.context.annotation.Configuration;
+
+import com.example.web.base.BaseApplicationConfig;
+
+@Configuration
+public class ApplicationConfig extends BaseApplicationConfig {
+
+}
---
+++ web/src/main/java/com/example/web/base/BaseApplicationConfig.java
@@ -0,0 +1,5 @@
+package com.example.web.base;
+
+public class BaseApplicationConfig {
+
+}
