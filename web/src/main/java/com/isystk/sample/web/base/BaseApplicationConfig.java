4d9ec4c12d51b06d2c6a719dae03bb5e55d4c6bf
iseyoshitaka <iseyoshitaka@team-lab.com>
2020-03-21 11:01:32 +0900

    AOPとログ出力が完成

--- web/src/main/java/com/isystk/sample/web/base/BaseApplicationConfig.java
+++ web/src/main/java/com/isystk/sample/web/base/BaseApplicationConfig.java
@@ -1,11 +1,15 @@
 package com.isystk.sample.web.base;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.isystk.sample.web.base.aop.LoggingFunctionNameInterceptor;
+import com.isystk.sample.web.base.aop.RequestTrackingInterceptor;
 
 public abstract class BaseApplicationConfig
 		implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>, WebMvcConfigurer {
@@ -14,10 +18,40 @@ public abstract class BaseApplicationConfig
 	public void customize(ConfigurableServletWebServerFactory container) {
 	}
 
+    @Bean
+    public RequestTrackingInterceptor requestTrackingInterceptor() {
+        // MDCにIDを設定してリクエストをトラッキングする
+        return new RequestTrackingInterceptor();
+    }
+
 	@Bean
 	public LoggingFunctionNameInterceptor loggingFunctionNameInterceptor() {
 		// MDCに機能名を設定してログに出力する
 		return new LoggingFunctionNameInterceptor();
 	}
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestTrackingInterceptor());
+        registry.addInterceptor(loggingFunctionNameInterceptor());
+    }
+
+	/**
+	 * [] を含むURLをGETしようとするとTomcatが400を返す 問題に対応
+	 * @return
+	 */
+	@Bean
+	public WebServerFactoryCustomizer<TomcatServletWebServerFactory>
+	    containerCustomizer(){
+	    return new EmbeddedTomcatCustomizer();
+	}
+	private static class EmbeddedTomcatCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+	    @Override
+	    public void customize(TomcatServletWebServerFactory factory) {
+	        factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+	            connector.setAttribute("relaxedPathChars", "<>[\\]^`{|}");
+	            connector.setAttribute("relaxedQueryChars", "<>[\\]^`{|}");
+	        });
+	    }
+	}
 }
