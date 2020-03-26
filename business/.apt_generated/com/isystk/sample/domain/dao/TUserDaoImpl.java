011361acb29a7aa2d0358c685d247a89b2d03739
iseyoshitaka <ise0615@gmail.com>
2020-03-26 12:13:24 +0900

    CSV以外のダウンロードが完成

--- business/.apt_generated/com/isystk/sample/domain/dao/TUserDaoImpl.java
+++ business/.apt_generated/com/isystk/sample/domain/dao/TUserDaoImpl.java
@@ -2,7 +2,7 @@ package com.isystk.sample.domain.dao;
 
 /** */
 @org.springframework.stereotype.Repository()
-@javax.annotation.Generated(value = { "Doma", "2.27.1" }, date = "2020-03-26T00:53:31.167+0900")
+@javax.annotation.Generated(value = { "Doma", "2.27.1" }, date = "2020-03-26T11:19:10.137+0900")
 public class TUserDaoImpl extends org.seasar.doma.internal.jdbc.dao.AbstractDao implements com.isystk.sample.domain.dao.TUserDao {
 
     static {
