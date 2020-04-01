1f846500373526921a8843a263a0658f6bcfdc7c
iseyoshitaka <iseyoshitaka@team-lab.com>
2020-04-01 13:16:50 +0900

    詳細画面とマイページのCSS追加

--- business/src/main/java/com/isystk/sample/solr/repository/SolrPostRepository.java
+++ business/src/main/java/com/isystk/sample/solr/repository/SolrPostRepository.java
@@ -3,13 +3,16 @@ package com.isystk.sample.solr.repository;
 
 import org.springframework.data.solr.repository.Query;
 import org.springframework.data.solr.repository.SolrCrudRepository;
+
+import com.isystk.sample.domain.dto.common.Page;
+import com.isystk.sample.domain.dto.common.Pageable;
 import com.isystk.sample.solr.dto.SolrPost;
 
 public interface SolrPostRepository extends SolrCrudRepository<SolrPost, String> {
 
-    @Query("post_id:** ?0** ")
+	@Query("post_id:?0")
     public SolrPost findByPostId(Integer postId);
-//
+
 //    @Query("id:** ?0**  OR title:** ?0** ")
 //    public Page<SolrPost> findByCustomQuery(String searchTerm, Pageable pageable);
 //
--- web-front/src/main/java/com/isystk/sample/web/front/controller/html/member/MemberHtmlController.java
+++ web-front/src/main/java/com/isystk/sample/web/front/controller/html/member/MemberHtmlController.java
@@ -9,7 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.isystk.sample.domain.dao.AuditInfoHolder;
+import com.isystk.sample.domain.dao.TUserDao;
 import com.isystk.sample.domain.dto.TPostCriteria;
+import com.isystk.sample.domain.dto.TUserCriteria;
+import com.isystk.sample.domain.dto.common.Pageable;
 import com.isystk.sample.web.front.service.PostService;
 import com.isystk.sample.web.base.controller.html.AbstractHtmlController;
 
@@ -23,6 +27,9 @@ public class MemberHtmlController extends AbstractHtmlController {
 
 	@Autowired
 	PostService postService;
+
+	@Autowired
+	TUserDao tUserDao;
 
 	@Override
 	public String getFunctionName() {
@@ -38,10 +45,14 @@ public class MemberHtmlController extends AbstractHtmlController {
 	@GetMapping()
 	public String index(@ModelAttribute MemberHtmlForm form, Model model) {
 		// 入力値を詰め替える
-		val criteria = modelMapper.map(form, TPostCriteria.class);
-
-		// 10件区切りで取得する
-		val pages = postService.findAll(criteria, form);
+		var inputPost = new TPostCriteria();
+		// ログインユーザーID
+		var criteria = new TUserCriteria();
+		criteria.setEmailEqual(AuditInfoHolder.getAuditUser());
+	    var tUser = tUserDao.select(criteria).orElseThrow();
+		inputPost.setUserId(tUser.getUserId());
+		// TODO 10件区切りで取得する
+		val pages = postService.findAll(inputPost, Pageable.NO_LIMIT);
 
 		// 画面に検索結果を渡す
 		model.addAttribute("pages", pages);
