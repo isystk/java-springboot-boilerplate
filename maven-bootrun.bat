000d9e6ce06c1db0d82dcda090d9ffa71c566ac6
iseyoshitaka <ise0615@gmail.com>
2020-04-06 20:39:26 +0900

    ドキュメントを整理

--- maven-bootrun.bat
+++ maven-bootrun.bat
@@ -8,8 +8,8 @@ set PATH=%JAVA_HOME%\bin;%PATH%
 set PATH=%MAVEN_HOME%\bin;%PATH%
 
 call cd %~dp0
-rem java -jar  -Dspring.profiles.active=local web-front/target/web-front.jar
-java -jar  -Dspring.profiles.active=staging web-front/target/web-front.jar
+java -jar  -Dspring.profiles.active=local web-front/target/web-front.jar
+rem java -jar  -Dspring.profiles.active=staging web-front/target/web-front.jar
 rem java -jar  -Dspring.profiles.active=production web-front/target/web-front.jar
 
 pause
