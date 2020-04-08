ff5aa9ef079564267f22ee7aa846767553fe96f5
iseyoshitaka <ise0615@gmail.com>
2020-04-08 11:46:12 +0900

    pom edit

--- maven-bootrun.bat
+++ maven-bootrun.bat
@@ -2,10 +2,8 @@
 
 set PJT_HOME=%~dp0\..\docker-java
 set JAVA_HOME=%PJT_HOME%\tools\java\11
-set MAVEN_HOME=%PJT_HOME%\tools\maven
 
 set PATH=%JAVA_HOME%\bin;%PATH%
-set PATH=%MAVEN_HOME%\bin;%PATH%
 
 call cd %~dp0
 java -jar  -Dspring.profiles.active=local web-front/target/web-front.jar
