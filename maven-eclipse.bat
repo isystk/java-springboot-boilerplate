875821272709e35ac814318eb158216b32ddd847
iseyoshitaka <iseyoshitaka@team-lab.com>
2020-04-08 11:46:12 +0900

    pom edit

--- maven-eclipse.bat
+++ maven-eclipse.bat
@@ -2,12 +2,10 @@
 
 set PJT_HOME=%~dp0\..\docker-java
 set JAVA_HOME=%PJT_HOME%\tools\java\11
-set MAVEN_HOME=%PJT_HOME%\tools\maven
 
 set PATH=%JAVA_HOME%\bin;%PATH%
-set PATH=%MAVEN_HOME%\bin;%PATH%
 
 call cd %~dp0
-call mvn -X -U eclipse:clean eclipse:eclipse -f pom.xml
+call mvnw -X -U eclipse:clean eclipse:eclipse -f pom.xml
 
 pause
--- maven-package.bat
+++ maven-package.bat
@@ -2,12 +2,10 @@
 
 set PJT_HOME=%~dp0\..\docker-java
 set JAVA_HOME=%PJT_HOME%\tools\java\11
-set MAVEN_HOME=%PJT_HOME%\tools\maven
 
 set PATH=%JAVA_HOME%\bin;%PATH%
-set PATH=%MAVEN_HOME%\bin;%PATH%
 
 call cd %~dp0
-call mvn clean package -Dmaven.test.skip=true
+call mvnw clean package -Dmaven.test.skip=true
 
 pause
