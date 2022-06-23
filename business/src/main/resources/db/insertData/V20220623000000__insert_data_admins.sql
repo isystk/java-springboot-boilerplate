3ddd9f1e26688fd88f66f539e49b77d39dbf06b9
iseyoshitaka <ise0615@gmail.com>
2022-06-23 18:18:35 +0900

    商品データを追加

---
+++ business/src/main/resources/db/insertData/V20220623000000__insert_data_admins.sql
@@ -0,0 +1,4 @@
+
+DELETE FROM admins WHERE email = 'sample@sample.com';
+INSERT INTO admins(name, email, password) VALUES
+('管理者A', 'sample@sample.com', '$2a$10$BkIISE3qUk3khS577JhW3OOuhJJ3LRo.xDZ9MfRuwkgq/TcTTWOyG');
--- business/src/main/resources/db/insertData/V20220623000000__insert_data_users.sql
+++ business/src/main/resources/db/insertData/V20220623000000__insert_data_users.sql
@@ -1,8 +1,4 @@
 
-DELETE FROM admins WHERE email = 'sample@sample.com';
-INSERT INTO admins(name, email, password) VALUES
-('管理者A', 'sample@sample.com', '$2a$10$BkIISE3qUk3khS577JhW3OOuhJJ3LRo.xDZ9MfRuwkgq/TcTTWOyG');
-
 DELETE FROM users WHERE email = 'test1@test.com';
 INSERT INTO users (name, email, password, email_verified_at, created_at, updated_at) VALUES
 ('テスト1', 'test1@test.com', '$2a$10$BkIISE3qUk3khS577JhW3OOuhJJ3LRo.xDZ9MfRuwkgq/TcTTWOyG', now(), now(), now());
