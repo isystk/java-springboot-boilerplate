4e98aeb06a3de991e316e3d0debe192b483d792f
iseyoshitaka <ise0615@gmail.com>
2022-07-03 22:27:32 +0900

    商品インデックスの生成に成功

---
+++ web-front/src/main/java/com/isystk/sample/web/front/controller/api/v1/post/StockRestForm.java
@@ -0,0 +1,14 @@
+package com.isystk.sample.web.front.controller.api.v1.post;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class StockRestForm implements Serializable {
+
+  private static final long serialVersionUID = 7593564324192730932L;
+
+}
