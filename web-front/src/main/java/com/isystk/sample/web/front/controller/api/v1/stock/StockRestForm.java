d67b40df4cc0c4f95a01c6baa5dcecfd2514bcf5
iseyoshitaka <ise0615@gmail.com>
2022-07-04 22:40:14 +0900

    商品一覧とページングが動いた

--- web-front/src/main/java/com/isystk/sample/web/front/controller/api/v1/stock/StockRestForm.java
+++ web-front/src/main/java/com/isystk/sample/web/front/controller/api/v1/stock/StockRestForm.java
@@ -1,14 +1,16 @@
 package com.isystk.sample.web.front.controller.api.v1.stock;
 
-import java.io.Serializable;
+import com.isystk.sample.common.dto.Pageable;
+import com.isystk.sample.web.base.controller.html.BaseSearchForm;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-public class StockRestForm implements Serializable {
+public class StockRestForm extends BaseSearchForm implements Pageable {
 
   private static final long serialVersionUID = 7593564324192730932L;
 
+  int perpage = 6;
 }
