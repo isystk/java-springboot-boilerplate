0488843a53939f3259b10b7c8fa5377fa487b7a1
iseyoshitaka <ise0615@gmail.com>
2022-07-14 18:13:21 +0900

    setup SolrJ

--- business/src/main/java/com/isystk/sample/solr/dto/SolrStock.java
+++ business/src/main/java/com/isystk/sample/solr/dto/SolrStock.java
@@ -1,102 +1,37 @@
 package com.isystk.sample.solr.dto;
 
 import com.isystk.sample.common.dto.Dto;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.solr.core.mapping.Indexed;
-import org.springframework.data.solr.core.mapping.SolrDocument;
+import java.util.Date;
+import lombok.Data;
+import org.apache.solr.client.solrj.beans.Field;
 
 import java.time.LocalDateTime;
 
-@SolrDocument(collection = "stock")
+@Data
 public class SolrStock implements Dto {
 
-  @Id
-  @Indexed(name = "id")
+  @Field("id")
   private String id;
 
-  @Indexed(name = "stock_id")
+  @Field("stock_id")
   private Integer stockId;
 
-  @Indexed(name = "name")
+  @Field("name")
   private String name;
 
-  @Indexed(name = "detail")
+  @Field("detail")
   private String detail;
 
-  @Indexed(name = "price")
+  @Field("price")
   private Integer price;
 
-  @Indexed(name = "imgpath")
+  @Field("imgpath")
   private String imgpath;
 
-  @Indexed(name = "quantity")
+  @Field("quantity")
   private Integer quantity;
 
-  @Indexed(name = "created_at")
-  private LocalDateTime createdAt;
+  @Field("created_at")
+  private Date createdAtDate;
 
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public Integer getStockId() {
-    return stockId;
-  }
-
-  public void setStockId(Integer stockId) {
-    this.stockId = stockId;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getDetail() {
-    return detail;
-  }
-
-  public void setDetail(String detail) {
-    this.detail = detail;
-  }
-
-  public Integer getPrice() {
-    return price;
-  }
-
-  public void setPrice(Integer price) {
-    this.price = price;
-  }
-
-  public String getImgpath() {
-    return imgpath;
-  }
-
-  public void setImgpath(String imgpath) {
-    this.imgpath = imgpath;
-  }
-
-  public Integer getQuantity() {
-    return quantity;
-  }
-
-  public void setQuantity(Integer quantity) {
-    this.quantity = quantity;
-  }
-
-  public LocalDateTime getCreatedAt() {
-    return createdAt;
-  }
-
-  public void setCreatedAt(LocalDateTime createdAt) {
-    this.createdAt = createdAt;
-  }
 }
